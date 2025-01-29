cf11ac6c3353e763be6a035f79ebd4cc90379d09
thabotwala <thabonjabulotwala@gmail.com>
2025-01-29 05:26:48 +0200

    *sigh*

--- plugin/example.lua
+++ plugin/example.lua
@@ -1,11 +1,1 @@
---basic lua scripting
-local M = {}
-
-function M.hello_world()
-  print("Hello, Wolrd!")
-end
-
---Map a command
-vim.api.nvim_command('command! HelloWorld lua require("example").hello_world()')
-
-return M;
+print("Hello, World")
